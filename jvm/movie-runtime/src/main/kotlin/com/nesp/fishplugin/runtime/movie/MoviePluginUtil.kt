98de04aa79866552c3e7089d5125020a19d5fba3
Jinzhaolu <1756404649@qq.com>
2022-04-06 23:18:52 +0800

    Support java8

--- jvm/movie-runtime/src/main/kotlin/com/nesp/fishplugin/runtime/movie/MoviePluginUtil.kt
+++ jvm/movie-runtime/src/main/kotlin/com/nesp/fishplugin/runtime/movie/MoviePluginUtil.kt
@@ -1,7 +1,7 @@
 package com.nesp.fishplugin.runtime.movie
 
-import com.nesp.fishplugin.core.data.Page
-import com.nesp.fishplugin.core.data.Plugin
+import com.nesp.fishplugin.core.data.Page2
+import com.nesp.fishplugin.core.data.Plugin2
 
 /**
  * @author <a href="mailto:1756404649@qq.com">JinZhaolu Email:1756404649@qq.com</a>
@@ -10,19 +10,19 @@ import com.nesp.fishplugin.core.data.Plugin
  **/
 object MoviePluginUtil {
 
-    fun findHomePage(plugin: Plugin): Page? {
+    fun findHomePage(plugin: Plugin2): Page2? {
         return plugin.findPageById(MoviePage.HOME.id)
     }
 
-    fun findCategoryPage(plugin: Plugin): Page? {
+    fun findCategoryPage(plugin: Plugin2): Page2? {
         return plugin.findPage { it.id.startsWith(MOVIE_PAGE_ID_CATEGORY) }
     }
 
-    fun findSearchPage(plugin: Plugin): Page? {
+    fun findSearchPage(plugin: Plugin2): Page2? {
         return plugin.findPageById(MoviePage.SEARCH.id)
     }
 
-    fun findDetailPage(plugin: Plugin): Page? {
+    fun findDetailPage(plugin: Plugin2): Page2? {
         return plugin.findPageById(MoviePage.DETAIL.id)
     }
 
