03a7e074158850f895890fc6a23223ea636a77d1
靳兆鲁 <1756404649@qq.com>
2022-04-11 01:06:39 +0800

    [Add] License and fdb

--- tools/editor/nesp-sdk-javafx/nesp-sdk-java/docs/package-search-index.js
+++ tools/editor/nesp-sdk-javafx/nesp-sdk-java/docs/package-search-index.js
@@ -1,1 +1,17 @@
+/*
+ * Copyright (c) 2022.  NESP Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 packageSearchIndex = [{"l":"com.nesp.sdk.java"},{"l":"com.nesp.sdk.java.annotation"},{"l":"com.nesp.sdk.java.ranges"},{"l":"com.nesp.sdk.java.util"},{"l":"所有程序包","url":"allpackages-index.html"}]
