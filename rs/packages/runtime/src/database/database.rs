8d5009ad21e50a9c92cb11dbb92dc0783f712228
Jack Jin <1756404649@qq.com>
2025-08-06 17:17:53 +0800

    feat(runtime): implement database connection and plugin manager
    - Add database module with SQLite connection functionality
    - Create plugin manager with basic structure and initialization
    - Define plugin source types and implement plugin installation from JSON manifest
    - Set up Cargo.toml with necessary dependencies
    - Create .gitignore file for the runtime package

---
+++ rs/packages/runtime/src/database/database.rs
@@ -0,0 +1,33 @@
+// Copyright (c) 2025. NESP Technology Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use std::time::Duration;
+
+use sea_orm::{ConnectOptions, Database, DatabaseConnection, DbErr};
+
+pub async fn open(path: &str) -> Result<DatabaseConnection, DbErr> {
+    // sqlite://path/to/db.sqlite?mode=rwc;
+    let path = format!("sqlite://{}?mode=rw", path);
+    let mut opt = ConnectOptions::new(path);
+    opt.max_connections(100)
+        .min_connections(5)
+        .connect_timeout(Duration::from_secs(8))
+        .acquire_timeout(Duration::from_secs(8))
+        .idle_timeout(Duration::from_secs(8))
+        .max_lifetime(Duration::from_secs(8))
+        .sqlx_logging(true)
+        .sqlx_logging_level(log::LevelFilter::Error);
+    let db = Database::connect(opt).await?;
+    Ok(db)
+}
---
+++ rs/packages/runtime/src/plugin_manager.rs
@@ -0,0 +1,74 @@
+// Copyright (c) 2025. NESP Technology Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use core::data::plugin::Plugin;
+use std::sync::{Arc, Mutex, OnceLock};
+
+use compiler::{json_plugin_compiler::JsonPluginCompiler, plugin_compiler::PluginCompiler};
+
+pub struct PluginManagerConfig {
+    pub database_path: String,
+}
+
+pub enum PluginSource {
+    ManifestJson(String),
+    ManifestJsonFile(String),
+}
+
+pub struct PluginManager {
+    config: Option<PluginManagerConfig>,
+}
+
+impl PluginManager {
+    fn get_instance() -> Arc<Mutex<PluginManager>> {
+        static INSTANCE: OnceLock<Arc<Mutex<PluginManager>>> = OnceLock::new();
+        INSTANCE
+            .get_or_init(|| Arc::new(Mutex::new(PluginManager { config: None })))
+            .clone()
+    }
+
+    pub fn init(&mut self, config: PluginManagerConfig) {
+        self.config = Some(config);
+    }
+
+    pub fn database_path(&self) -> String {
+        todo!()
+    }
+
+    pub fn install_plugin(&self, source: &PluginSource) -> Result<(), String> {
+        match source {
+            PluginSource::ManifestJson(json) => self.install_plugin_from_manifest_json(json),
+            PluginSource::ManifestJsonFile(path) => todo!(),
+        }
+    }
+
+    fn install_plugin_from_manifest_json(&self, json: &String) -> Result<(), String> {
+        let compiler_result = JsonPluginCompiler::parse(json);
+        if let Err(error) = compiler_result {
+            return Err(error.to_string());
+        }
+        let compiler = compiler_result.unwrap();
+        self.do_install_plugin(&compiler.compile()?);
+        Ok(())
+    }
+
+    fn do_install_plugin(&self, plugin: &Plugin) -> Result<(), String> {
+        todo!()
+    }
+
+    /// Uninstall a plugin by id
+    pub fn uninstall_plugin(&self, id: &str) -> Result<(), String> {
+        todo!()
+    }
+}
