1993186534726fb9156468ee2c653b73b3a2f764
Jack Jin <1756404649@qq.com>
2025-08-06 09:43:58 +0800

    refactor(compiler): update copyright and license information
    - Update copyright year to 2025
    - Replace proprietary license with Apache License 2.0
    - Update license header in multiple source files

--- rs/packages/core/src/data/screen_type.rs
+++ rs/packages/core/src/data/screen_type.rs
@@ -1,14 +1,18 @@
 use std::hash::Hash;
 
-// Copyright (c) 2025. NESP Technology Corporation. All rights reserved.
-// 
-// This program is not free software; you can't redistribute it and/or modify it
-// without the permit of team manager.
-// 
-// Unless required by applicable law or agreed to in writing.
-// 
-// If you have any questions or if you find a bug,
-// please contact the author by email or ask for Issues.
+// Copyright (c) 2025. NESP Technology Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 #[derive(Debug, Clone)]
 pub enum ScreenType {
     Compact,
