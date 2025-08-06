602757b23710ff17381d05dbfa72d3b354d3a1f8
Jack Jin <1756404649@qq.com>
2025-08-06 09:42:03 +0800

    refactor: update copyright notice across multiple files
    - Replace C-style comments with Rust-style line comments for consistency
    - Update copyright year to 2025 in all modified files
    - Adjust formatting of the copyright notice to match Rust conventions

--- rs/packages/core/src/data/screen_type.rs
+++ rs/packages/core/src/data/screen_type.rs
@@ -1,16 +1,14 @@
 use std::hash::Hash;
 
-/*
-* Copyright (c) 2025. NESP Technology Corporation. All rights reserved.
-*
-* This program is not free software; you can't redistribute it and/or modify it
-* without the permit of team manager.
-*
-* Unless required by applicable law or agreed to in writing.
-*
-* If you have any questions or if you find a bug,
-* please contact the author by email or ask for Issues.
-*/
+// Copyright (c) 2025. NESP Technology Corporation. All rights reserved.
+// 
+// This program is not free software; you can't redistribute it and/or modify it
+// without the permit of team manager.
+// 
+// Unless required by applicable law or agreed to in writing.
+// 
+// If you have any questions or if you find a bug,
+// please contact the author by email or ask for Issues.
 #[derive(Debug, Clone)]
 pub enum ScreenType {
     Compact,
