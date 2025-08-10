150247e766775e870fa11f393fee4ab9ba2f93c1
Jack Jin <1756404649@qq.com>
2025-08-10 11:56:40 +0800

    feat(runtime): add JavaObject struct for managing JNI objects
    - Create JavaObject struct to encapsulate JNIEnv and JObject
    - Implement new() method to create JavaObject instances
    - Add r#use() method to access JNIEnv and JObject
    - Implement Clone trait for JavaObject

--- rs/packages/runtime/src/jni/jni_utils.rs
+++ rs/packages/runtime/src/jni/jni_utils.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use jni::JNIEnv;
+use jni::{JNIEnv, objects::JObject};
 
 pub(crate) fn throw_java_expception_if_error<T>(
     env: &mut JNIEnv,
@@ -27,4 +27,30 @@ pub(crate) fn throw_java_expception_if_error<T>(
 
 pub(crate) fn throw_java_expception_msg(env: &mut JNIEnv, msg: &str) {
     env.throw_new("java/lang/Exception", msg).unwrap();
+}
+
+pub struct JavaObject<'a> {
+    env: JNIEnv<'a>,
+    object: JObject<'a>,
+}
+
+impl<'a> JavaObject<'a> {
+    pub fn new(env: &JNIEnv<'a>, object: &JObject<'a>) -> Self {
+        JavaObject {
+            env: unsafe { env.unsafe_clone() },
+            object: unsafe { JObject::from_raw(**object) },
+        }
+    }
+    pub fn r#use(&mut self) -> (&mut JNIEnv<'a>, &JObject<'a>) {
+        (&mut self.env, &self.object)
+    }
+}
+
+impl Clone for JavaObject<'_> {
+    fn clone(&self) -> Self {
+        Self {
+            env: unsafe { self.env.unsafe_clone() },
+            object: unsafe { JObject::from_raw(*self.object) },
+        }
+    }
 }
