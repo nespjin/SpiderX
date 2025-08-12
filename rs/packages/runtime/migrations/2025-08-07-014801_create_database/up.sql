af3efea7048b4e88783bbcc1ef50a900517200a9
Jack Jin <1756404649@qq.com>
2025-08-12 12:06:35 +0800

    refactor(database): reformat SQL statements for better readability
    - Indent SQL statements consistently
    - Align columns in CREATE TABLE statements
    - Add blank lines for improved readability
    - Update database schema file and migration script

--- rs/packages/runtime/migrations/2025-08-07-014801_create_database/up.sql
+++ rs/packages/runtime/migrations/2025-08-07-014801_create_database/up.sql
@@ -1,34 +1,39 @@
 CREATE TABLE IF NOT EXISTS "plugin" (
-	"id" VARCHAR NOT NULL UNIQUE,
-	"name" VARCHAR NOT NULL,
-	"author" VARCHAR,
-	"version" VARCHAR NOT NULL,
-	"runtime_version" VARCHAR NOT NULL,
-	"description" VARCHAR,
-	"tags" VARCHAR,
-	"supported_screen_types" VARCHAR,
-	PRIMARY KEY("id")
+   "id"                     varchar NOT NULL UNIQUE,
+   "name"                   varchar NOT NULL,
+   "author"                 varchar,
+   "version"                varchar NOT NULL,
+   "runtime_version"        varchar NOT NULL,
+   "description"            varchar,
+   "tags"                   varchar,
+   "supported_screen_types" varchar,
+   PRIMARY KEY ( "id" )
 );
 
-CREATE INDEX IF NOT EXISTS "plugin_index_0"
-ON "plugin" ("id");
+CREATE INDEX IF NOT EXISTS "plugin_index_0" ON
+   "plugin" (
+      "id"
+   );
 CREATE TABLE IF NOT EXISTS "dataset" (
-	"id" VARCHAR NOT NULL,
-	"plugin_id" VARCHAR NOT NULL,
-	"url" VARCHAR NOT NULL,
-	"url_compact" VARCHAR,
-	"url_medium" VARCHAR,
-	"url_expanded" VARCHAR,
-	"js" TEXT,
-	"js_compact" TEXT,
-	"js_medium" TEXT,
-	"js_expanded" TEXT,
-	"dsl_default" TEXT,
-	"dsl_compact" TEXT,
-	"dsl_medium" TEXT,
-	"dsl_expanded" TEXT,
-	PRIMARY KEY (id, plugin_id)
+   "id"           varchar NOT NULL,
+   "plugin_id"    varchar NOT NULL,
+   "url"          varchar NOT NULL,
+   "url_compact"  varchar,
+   "url_medium"   varchar,
+   "url_expanded" varchar,
+   "js"           text,
+   "js_compact"   text,
+   "js_medium"    text,
+   "js_expanded"  text,
+   "dsl_default"  text,
+   "dsl_compact"  text,
+   "dsl_medium"   text,
+   "dsl_expanded" text,
+	PRIMARY KEY ( "id" , "plugin_id" )
 );
 
-CREATE INDEX IF NOT EXISTS "dataset_index_0"
-ON "dataset" ("id", "plugin_id");
+CREATE INDEX IF NOT EXISTS "dataset_index_0" ON
+   "dataset" (
+      "id",
+      "plugin_id"
+   );
