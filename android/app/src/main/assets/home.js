540618322671363f47634dad8a273a7fe0b8f6f4
Jack Jin <1756404649@qq.com>
2026-04-07 01:10:02 +0800

    feat(home): add movie data extraction from kkcechi.com homepage
    - Implement script to parse slider movies with name, cover image, and detail link
    - Extract multiple sections: newPlay, newMovie, newSoap, newVariety, newAnim movies
    - Structure extracted data into a page object for comprehensive access
    - Update plugin.json to point to kkcechi.com and embed the extraction script as expanded js function

---
+++ android/app/src/main/assets/home.js
@@ -0,0 +1,51 @@
+// kkcechi.com
+(function () {
+    const sliders = document.querySelectorAll('#swiper-wrapper-a4ac78673ec105de7 > div');
+    const sliderMovies = [];
+
+    for (const slider of sliders) {
+        sliderMovies.push(
+            {
+                name: slider.querySelector('h2').innerText,
+                coverImageUrl: slider.querySelector('img').src,
+                detail: slider.querySelector('a.poster-img').href
+            }
+        );
+    }
+
+    function querySectionMovies(query) {
+        const elements = document.querySelectorAll(query);
+        const movies = [];
+
+        for (const newPlay of elements) {
+            const div = newPlay.querySelector('div.cover-data');
+            const a = div.querySelector('h4 > a');
+            movies.push(
+                {
+                    name: a.innerText,
+                    coverImageUrl: newPlay.querySelector('img').src,
+                    detail: a.href,
+                    status: div.querySelector('p.text').innerText
+                }
+            );
+        }
+        return movies;
+    }
+
+    const newPlayMovies = querySectionMovies('body > div.page > div > div.page-row > div:nth-child(3) > div.panel-bd > ul > li');
+    const newMovies = querySectionMovies('body > div.page > div > div.page-row > div:nth-child(5) > div.panel-bd > ul > li');
+    const newSoaps = querySectionMovies('body > div.page > div > div.page-row > div:nth-child(6) > div.panel-bd > ul > li');
+    const newVariety = querySectionMovies('body > div.page > div > div.page-row > div:nth-child(8) > div.panel-bd > ul > li');
+    const newAnim = querySectionMovies('body > div.page > div > div.page-row > div:nth-child(6) > div.panel-bd > ul > li');
+
+    const page = {
+        slideMovies: sliderMovies,
+        newPlay: newPlayMovies,
+        newMovie: newMovies,
+        newSoap: newSoaps,
+        newVariety: newVariety,
+        newAnim: newAnim
+    };
+
+    return page;
+})()
