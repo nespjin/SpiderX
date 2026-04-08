37f1e54d07ef3f2af95489fb7d6d7a520afb06c7
Jack Jin <1756404649@qq.com>
2026-04-09 00:59:07 +0800

    refactor: rename core package to spiderx_core
    - Rename core package to spiderx_core in Cargo.toml files
    - Update import statements from core::data to spiderx_core::data
    - Modify dependency references from core to spiderx_core across packages
    - Update package name in runtime dependencies to use new naming convention
    
    Signed-off-by: Jack Jin <1756404649@qq.com>

--- rs/packages/compiler/src/json_plugin_compiler.rs
+++ rs/packages/compiler/src/json_plugin_compiler.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use core::data::{
+use spiderx_core::data::{
     json::plugin_manifest_json_data::PluginManifestJsonData,
     plugin::{Dataset, Plugin},
     screen_type::ScreenType,
--- rs/packages/compiler/src/plugin_compiler.rs
+++ rs/packages/compiler/src/plugin_compiler.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use core::data::plugin::Plugin;
+use spiderx_core::data::plugin::Plugin;
 
 pub trait PluginCompiler {
     fn compile(&self) -> Result<Plugin, String>;
