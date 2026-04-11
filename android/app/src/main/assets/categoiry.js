86e9f031cf5e3cd88f49dfc0006bc9bd8b2aca6d
Jack Jin <1756404649@qq.com>
2026-04-11 17:28:11 +0800

    fix(parser resolve): image URL parsing in movie data extraction
    - Updated cover image URL construction to use origin URL with data-original attribute
    - Fixed regex pattern escaping for digit matching in pagination logic
    - Removed unnecessary semicolons and standardized code formatting
    - Improved image source handling from lazy loading attributes

--- android/app/src/main/assets/categoiry.js
+++ android/app/src/main/assets/categoiry.js
@@ -1,52 +1,46 @@
 (function () {
-    const groupEles = document.querySelector('body > div.page > div > div.page-row > div:nth-child(3) > div').querySelectorAll('div.shoutu-screen');
-    const groups = [];
-
-    for (const ele of groupEles) {
-        const categoryEles = ele.querySelectorAll('li.swiper-slide');
-        const categories = [...categoryEles].map((item) => item.querySelector('a'))
-            .filter((item) => item)
-            .map((item) => ({ title: item.innerText, pageUrl: item.href, }));
-        groups.push(
-            {
-                title: ele.querySelector('div:nth-child(1)').innerText,
-                categories: categories
-            }
-        );
-    }
-
-    const movieEles = document.querySelector('body > div.page > div > div.page-row > div:nth-child(4) > div.panel-bd > ul.shoutu-vodlist').querySelectorAll('li');
-    const movies = [];
-    for (const ele of movieEles) {
-        const a = ele.querySelector('a');
-        movies.push(
-            {
-                name: a.title,
-                coverImageUrl: ele.querySelector('img').src,
-                detail: a.href,
-                status: ele.querySelector('p.text').innerText
-            }
-        );
-    }
-
-    let nextPageUrl = '';
-    const pageContainer = document.querySelector('body > div.page > div > div.page-row > div:nth-child(4) > div.panel-bd > ul.shoutu-page.text-center');
-    if (pageContainer) {
-        const pagePos = pageContainer.querySelector('li.visible-xs').innerText;
-
-        if (pagePos.match(/\d+/)[0] < pagePos.match(/\d+/g)[1]) {
-            const pageEles = pageContainer.querySelectorAll('li');
-            for (const ele of pageEles) {
-                if (ele.innerText === '下一页') {
-                    nextPageUrl = ele.querySelector('a').href;
-                    break;
-                }
-            }
-        }
-    }
-    return {
-        movieCategoryGroups: groups,
-        movies: movies,
-        nextPageUrl: nextPageUrl
-    };
+	const groupEles = document.querySelector('body > div.page > div > div.page-row > div:nth-child(3) > div').querySelectorAll('div.shoutu-screen');
+	const groups = [];
+	for (const ele of groupEles) {
+		const categoryEles = ele.querySelectorAll('li.swiper-slide');
+		const categories = [...categoryEles].map((item) => item.querySelector('a')).filter((item) => item).map((item) => ({
+			title: item.innerText,
+			pageUrl: item.href,
+		}));
+		groups.push({
+			title: ele.querySelector('div:nth-child(1)').innerText,
+			categories: categories
+		})
+	}
+	const movieEles = document.querySelector('body > div.page > div > div.page-row > div:nth-child(4) > div.panel-bd > ul.shoutu-vodlist').querySelectorAll('li');
+	const movies = [];
+	const originUrl = window.location.origin;
+	for (const ele of movieEles) {
+		const a = ele.querySelector('a');
+		movies.push({
+			name: a.title,
+			coverImageUrl: originUrl + ele.querySelector('img').getAttribute('data-original'),
+			detail: a.href,
+			status: ele.querySelector('p.text').innerText
+		})
+	}
+	let nextPageUrl = '';
+	const pageContainer = document.querySelector('body > div.page > div > div.page-row > div:nth-child(4) > div.panel-bd > ul.shoutu-page.text-center');
+	if (pageContainer) {
+		const pagePos = pageContainer.querySelector('li.visible-xs').innerText;
+		if (pagePos.match(/\\d+/)[0] < pagePos.match(/\\d+/g)[1]) {
+			const pageEles = pageContainer.querySelectorAll('li');
+			for (const ele of pageEles) {
+				if (ele.innerText === '下一页') {
+					nextPageUrl = ele.querySelector('a').href;
+					break
+				}
+			}
+		}
+	}
+	return {
+		movieCategoryGroups: groups,
+		movies: movies,
+		nextPageUrl: nextPageUrl
+	}
 })()
